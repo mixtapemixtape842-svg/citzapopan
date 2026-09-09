1c62ec7a07364dfd2b2b7d9bb11842b622df0298
mixtapemixtape842-svg <mixtapemixtape842@gmail.com>
2026-09-08 23:31:37 -0600

    Add files via upload

---
+++ MAscotas virtuales/alumnos.js
@@ -0,0 +1,67 @@
+const alumnos = {
+
+    daniel: {
+        password: "1234",
+        nombre: "Daniel",
+
+        // Mascota
+        mascota: null,
+
+        // Calificación
+        calificacion: 10,
+
+        // Actividades
+        actividades: 1,
+        actividadesTotales: 1,
+
+        // Tareas
+        tareas: 1,
+        tareasTotales: 1
+    },
+
+    alumno02: {
+        password: "1234",
+        nombre: "Alumno 02",
+
+        mascota: null,
+
+        calificacion: 7,
+
+        actividades: 10,
+        actividadesTotales: 12,
+
+        tareas: 6,
+        tareasTotales: 8
+    },
+
+    alumno03: {
+        password: "1234",
+        nombre: "Alumno 03",
+
+        mascota: null,
+
+        calificacion: 7,
+
+        actividades: 8,
+        actividadesTotales: 12,
+
+        tareas: 5,
+        tareasTotales: 8
+    },
+
+  alumno04: {
+        password: "1234",
+        nombre: "Alumno 04",
+
+        mascota: null,
+
+        calificacion: 5,
+
+        actividades: 8,
+        actividadesTotales: 12,
+
+        tareas: 5,
+        tareasTotales: 8
+    },
+
+};
